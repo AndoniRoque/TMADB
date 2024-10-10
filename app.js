867712f7665b4d29699e8ae2715c76801b014bf1
AndoniRoque <roque.andoni@gmail.com>
2024-10-10 11:43:56 -0300

    query params 2

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
 
-/**
- * GET /odin/messages will have this log
- * { username: 'odin' }
- *
- * GET /theodinproject79687378/messages would instead log
- * { username: 'theodinproject79687378' }
- */
-app.get("/:username/messages", (req, res) => {
-  console.log(req.params);
-  res.send(`hola, ${req.params.username}`);
-});
+// /**
+//  * GET /odin/messages will have this log
+//  * { username: 'odin' }
+//  *
+//  * GET /theodinproject79687378/messages would instead log
+//  * { username: 'theodinproject79687378' }
+//  */
+// app.get("/:username/messages", (req, res) => {
+//   console.log(req.params);
+//   res.send(`hola, ${req.params.username}`);
+// });
 
 /**
  * GET /odin/messages/79687378 will have this log
@@ -20,6 +20,21 @@ app.get("/:username/messages", (req, res) => {
 app.get("/:username/messages/:messageId", (req, res) => {
   console.log(req.params);
   res.send(`${req.params.username} : ${req.params.messageId}`);
+});
+
+/**
+ * GET /odin/messages?sort=date&direction=ascending will log
+ * Params: { username: "odin" }
+ * Query: { sort: "date", direction: "ascending" }
+ *
+ * GET /odin/messages?sort=date&sort=likes&direction=ascending will log
+ * Params: { username: "odin" }
+ * Query: { sort: ["date", "likes"], direction: "ascending" }
+ */
+app.get("/:username/messages", (req, res) => {
+  console.log("Params:", req.params);
+  console.log("Query:", req.query);
+  res.send(`${req.params.username} - ${req.query.sort[1]}, ${req.query.direction}`);
 });
 
 const PORT = 3333;
