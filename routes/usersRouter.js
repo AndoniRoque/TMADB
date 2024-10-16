90cbb96de7d12df4f22651ac88cf52423b1ccbd2
AndoniRoque <roque.andoni@gmail.com>
2024-10-16 12:38:36 -0300

    cheat sheet

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -10,3 +10,8 @@ usersRouter.post("/:id/update", usersController.usersUpdatePost);
 usersRouter.post("/:id/delete", usersController.usersDeletePost);
 
 module.exports = usersRouter;
+
+
+// router -> define the routes of the web app and what method to use from the controller
+// controller -> define the logic on that method, what to do with the info. 
+// view -> show the info. 
