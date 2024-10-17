5f1f8efcc9360e842dd6802a21b3b755d315dd61
AndoniRoque <roque.andoni@gmail.com>
2024-10-17 13:46:26 -0300

    Log in con session done!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 const PORT = process.env.PORT || 3333;
 
 const pool = new Pool({
@@ -21,7 +21,9 @@ app.use(session({secret: "cats", resave: false, saveUninitialized: false}));
 app.use(passport.session());
 app.use(express.urlencoded({extended: false}));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user});
+})
 app.get('/sign-up', (req, res) => res.render("createUser"));
 app.post("/sign-up", async (req, res, next) => {
   try {
@@ -34,7 +36,58 @@ app.post("/sign-up", async (req, res, next) => {
     return next(err);
   }
 });
+app.post("/log-in", passport.authenticate("local", { 
+  successRedirect: "/",
+  failureRedirect: "/"
+}));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if(err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      console.log(rows);
+      const user = rows[0];
 
+      if(!user) {
+        console.log("Incorrect username");
+        return done(null, false, {message: "Incorrect username"});
+      }
+      if(user.password !== password) {
+        console.log("Incorrect password");
+        return done(null, false, {message: "Incorrect password"});
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+)
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+})
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
 
 app.listen(PORT, () =>  console.log(`Express app listening on port ${PORT}`));
