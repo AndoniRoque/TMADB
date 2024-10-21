3184dfed17a3d25cc6ff00b478a8bb92a8bd0bd4
AndoniRoque <roque.andoni@gmail.com>
2024-10-21 09:39:50 -0300

    leaving monolith

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const PORT = process.env.PORT || 3333;
 
+
+// ${{ Postgres.DATABASE_URL }}
+
 const pool = new Pool({
-  host: "localhost",
-  user: "mesa20",
-  database: "top_users",
-  password: "mesa20",
+  host: "postgres.railway.internal",
+  user: "postgres",
+  database: "railway",
+  password: "BTrxblTpBITXRFrNJCIhncJnXRZlgmnJ",
   port: 5432,
 })
 
@@ -24,70 +27,70 @@ app.use(express.urlencoded({extended: false}));
 app.get("/", (req, res) => {
   res.render("index", { user: req.user});
 })
-app.get('/sign-up', (req, res) => res.render("createUser"));
-app.post("/sign-up", async (req, res, next) => {
-  try {
-    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-      req.body.username,
-      req.body.password,
-    ]);
-    res.redirect("/");
-  } catch(err) {
-    return next(err);
-  }
-});
-app.post("/log-in", passport.authenticate("local", { 
-  successRedirect: "/",
-  failureRedirect: "/"
-}));
-app.use((req, res, next) => {
-  res.locals.currentUser = req.user;
-  next();
-});
-app.get("/log-out", (req, res, next) => {
-  req.logout((err) => {
-    if(err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
+// app.get('/sign-up', (req, res) => res.render("createUser"));
+// app.post("/sign-up", async (req, res, next) => {
+//   try {
+//     await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+//       req.body.username,
+//       req.body.password,
+//     ]);
+//     res.redirect("/");
+//   } catch(err) {
+//     return next(err);
+//   }
+// });
+// app.post("/log-in", passport.authenticate("local", { 
+//   successRedirect: "/",
+//   failureRedirect: "/"
+// }));
+// app.use((req, res, next) => {
+//   res.locals.currentUser = req.user;
+//   next();
+// });
+// app.get("/log-out", (req, res, next) => {
+//   req.logout((err) => {
+//     if(err) {
+//       return next(err);
+//     }
+//     res.redirect("/");
+//   });
+// });
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-      console.log(rows);
-      const user = rows[0];
+// passport.use(
+//   new LocalStrategy(async (username, password, done) => {
+//     try {
+//       const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+//       console.log(rows);
+//       const user = rows[0];
 
-      if(!user) {
-        console.log("Incorrect username");
-        return done(null, false, {message: "Incorrect username"});
-      }
-      if(user.password !== password) {
-        console.log("Incorrect password");
-        return done(null, false, {message: "Incorrect password"});
-      }
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  })
-)
+//       if(!user) {
+//         console.log("Incorrect username");
+//         return done(null, false, {message: "Incorrect username"});
+//       }
+//       if(user.password !== password) {
+//         console.log("Incorrect password");
+//         return done(null, false, {message: "Incorrect password"});
+//       }
+//       return done(null, user);
+//     } catch (err) {
+//       return done(err);
+//     }
+//   })
+// )
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-})
+// passport.serializeUser((user, done) => {
+//   done(null, user.id);
+// })
 
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    const user = rows[0];
+// passport.deserializeUser(async (id, done) => {
+//   try {
+//     const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+//     const user = rows[0];
 
-    done(null, user);
-  } catch(err) {
-    done(err);
-  }
-});
+//     done(null, user);
+//   } catch(err) {
+//     done(err);
+//   }
+// });
 
 app.listen(PORT, () =>  console.log(`Express app listening on port ${PORT}`));
--- db/queries.js
+++ db/queries.js
@@ -1,13 +1,13 @@
 const pool = require("./pool");
 
 async function getAllUsernames() {
-  const { rows } = await pool.query("SELECT * FROM usernames");
+  const { rows } = await pool.query("SELECT * FROM characters");
   return rows;
 }
 
-async function insertUsername(username) {
-  await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
-}
+// async function insertUsername(username) {
+//   await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
+// }
 
 module.exports = {
   getAllUsernames,
