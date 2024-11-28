d9c497c499a7cd383f4df43283049a4527e7ccf7
AndoniRoque <roque.andoni@gmail.com>
2024-11-28 10:39:06 -0300

    validation for episodes

--- back/src/routes/episodes.routes.js
+++ back/src/routes/episodes.routes.js
@@ -4,64 +4,96 @@ import { ensureAuthenticated } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-router.get('/episodes', ensureAuthenticated, async (req, res) => {
+router.get("/episodes", ensureAuthenticated, async (req, res) => {
   const { title, number, caseNumber } = req.query;
 
   try {
     const episodes = await prisma.episode.findMany({
       where: {
-        ...(title ? { title: { contains: title, mode: 'insensitive' } } : {}),
+        ...(title ? { title: { contains: title, mode: "insensitive" } } : {}),
         ...(number ? { number: parseInt(number) } : {}),
-        ...(caseNumber ? { caseNumber } : {})
+        ...(caseNumber ? { caseNumber } : {}),
       },
       orderBy: {
-        number: 'asc'
+        number: "asc",
       },
       include: {
         characters: {
           include: {
             character: true,
-          }
+          },
         },
-      }
+      },
     });
 
     if (!episodes || episodes.length === 0) {
-      return res.status(200).json({ message: "There are no uploaded episodes yet." });
+      return res
+        .status(200)
+        .json({ message: "There are no uploaded episodes yet." });
     }
 
     res.status(200).json(episodes);
   } catch (err) {
-    res.status(500).json({ error: 'Error getting episodes', details: err.message })
+    res
+      .status(500)
+      .json({ error: "Error getting episodes", details: err.message });
   }
-})
-router.post('/episodes', async (req, res) => {
-  const { title, number, releaseDate, description, caseNumber, season, heard, characterIds } = req.body;
+});
+router.post("/episodes", async (req, res) => {
+  const {
+    title,
+    number,
+    releaseDate,
+    description,
+    caseNumber,
+    season,
+    heard,
+    characterIds,
+  } = req.body;
 
   try {
     if (!title) return res.status(400).json({ message: "Title is missing" });
-    if (!number) return res.status(400).json({ message: "Episode number is missing" });
-    if (!Number.isInteger(number)) return res.status(400).json({message: "Episode number must be an integer."});
-    if (!releaseDate) return res.status(400).json({ message: "Release date is missing." });
-    if (!description) return res.status(400).json({ message: "The episode description is missing." });
-    if (!caseNumber) return res.status(400).json({ message: "Case number is missing." });
-
+    if (!number)
+      return res.status(400).json({ message: "Episode number is missing" });
+    if (!Number.isInteger(number))
+      return res
+        .status(400)
+        .json({ message: "Episode number must be an integer." });
+    if (!releaseDate)
+      return res.status(400).json({ message: "Release date is missing." });
+    if (!description)
+      return res
+        .status(400)
+        .json({ message: "The episode description is missing." });
+    if (!caseNumber)
+      return res.status(400).json({ message: "Case number is missing." });
 
     const existingEpisode = await prisma.episode.findFirst({
       where: {
-        title: { equals: title, mode: 'insensitive' },
-        number: { equals: number },
-        releaseDate: { equals: releaseDate },
-        caseNumber: { equals: caseNumber }
-      }
+        OR: [
+          { title: { equals: title, mode: "insensitive" } },
+          { number: number },
+          { releaseDate: releaseDate },
+          { caseNumber: caseNumber },
+        ],
+      },
     });
 
-    if (existingEpisode) return res.status(409).json({ message: "An episode with these values already exists, please upload a new one." });
+    if (existingEpisode)
+      return res.status(409).json({
+        message:
+          "An episode with these values already exists, please upload a new one.",
+      });
 
     let normilizedTitle = "";
 
     function capitalizeValues(value) {
-      return value.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+      return value
+        .split(" ")
+        .map(
+          (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+        )
+        .join(" ");
     }
 
     normilizedTitle = capitalizeValues(title);
@@ -76,30 +108,32 @@ router.post('/episodes', async (req, res) => {
         season,
         heard,
         characters: {
-          create: characterIds.map(characterId => ({
+          create: characterIds.map((characterId) => ({
             character: {
               connect: {
-                id: characterId
-              }
-            }
-          }))
-        }
+                id: characterId,
+              },
+            },
+          })),
+        },
       },
       include: {
         characters: {
           include: {
-            character: true
-          }
-        }
-      }
+            character: true,
+          },
+        },
+      },
     });
     res.status(200).json(newEpisode);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Error getting episodes', details: err.message });
+    res
+      .status(500)
+      .json({ error: "Error getting episodes", details: err.message });
   }
-})
-router.get('/episodes/:id', async (req, res) => {
+});
+router.get("/episodes/:id", async (req, res) => {
   const id = parseInt(req.params.id, 10);
 
   if (isNaN(id)) {
@@ -113,19 +147,32 @@ router.get('/episodes/:id', async (req, res) => {
         characters: {
           include: {
             character: true,
-          }
+          },
         },
-      }
-    })
+      },
+    });
 
-    !episode ? res.status(400).json({ error: 'Episode not found.' }) : res.status(200).json(episode);
+    !episode
+      ? res.status(400).json({ error: "Episode not found." })
+      : res.status(200).json(episode);
   } catch (err) {
-    res.status(500).json({ error: 'Error getting episodes', details: err.message })
+    res
+      .status(500)
+      .json({ error: "Error getting episodes", details: err.message });
   }
-})
-router.put('/episodes/:id', async (req, res) => {
+});
+router.put("/episodes/:id", async (req, res) => {
   const { id } = req.params;
-  const { title, number, releaseDate, description, heard, caseNumber, season, characterIds = [] } = req.body;
+  const {
+    title,
+    number,
+    releaseDate,
+    description,
+    heard,
+    caseNumber,
+    season,
+    characterIds = [],
+  } = req.body;
 
   if (isNaN(parseInt(id, 10))) {
     return res.status(400).json({ error: "Invalid ID format" });
@@ -142,8 +189,6 @@ router.put('/episodes/:id', async (req, res) => {
       (characterId) => !characterIds.includes(characterId)
     );
 
-    console.log(charactersToDisconnect);
-
     await prisma.episodesOnCharacters.deleteMany({
       where: {
         episodeId: parseInt(id),
@@ -156,7 +201,7 @@ router.put('/episodes/:id', async (req, res) => {
     );
 
     console.log("Characters to connect > ", charactersToConnect);
-    
+
     await prisma.episodesOnCharacters.createMany({
       data: charactersToConnect.map((characterId) => ({
         episodeId: parseInt(id),
@@ -184,32 +229,37 @@ router.put('/episodes/:id', async (req, res) => {
 
     res.status(200).json(updatedEpisode);
   } catch (err) {
-    res.status(500).json({ error: "Error updating episode", details: err.message });
+    res
+      .status(500)
+      .json({ error: "Error updating episode", details: err.message });
   }
 });
-router.delete('/episodes/:id', async (req, res) => {
+router.delete("/episodes/:id", async (req, res) => {
   const id = parseInt(req.params.id, 10);
 
-  if (isNaN(id)) { return res.send(400).json({ error: "Invalid ID format." }) }
+  if (isNaN(id)) {
+    return res.send(400).json({ error: "Invalid ID format." });
+  }
 
   try {
     await prisma.episodesOnCharacters.deleteMany({
       where: {
         episodeId: id,
-      }
+      },
     });
 
     await prisma.episode.delete({
       where: {
-        id: id
-      }
-    })
+        id: id,
+      },
+    });
 
     res.status(200).json({ message: "The episode was deleted successfully" });
   } catch (err) {
-    res.status(500).json({ error: "Error deleting episode", details: err.message });
+    res
+      .status(500)
+      .json({ error: "Error deleting episode", details: err.message });
   }
-})
+});
 
 export default router;
-
